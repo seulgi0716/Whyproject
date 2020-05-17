e329037f74b700a2ce98d063447f586b533a780a
leeseulgi <ijk9877@naver.com>
2020-05-17 22:26:05 +0900

    2020-05-17 commit_5

--- app/src/main/java/com/example/whyproject/Trashcan.java
+++ app/src/main/java/com/example/whyproject/Trashcan.java
@@ -2,14 +2,68 @@ package com.example.whyproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
+import android.os.Message;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Trashcan extends AppCompatActivity {
+
+    private EditText writetrash;
+    private TextView papertv;
+    private ImageButton throwbtn;
+    private ImageView trashcan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trashcan);
         setTitle("괜찮아 다 말해봐!");
+
+        writetrash = findViewById(R.id.writetrash);
+        papertv = findViewById(R.id.papertv);
+        throwbtn = findViewById(R.id.throwbtn);
+        trashcan = findViewById(R.id.trashcan);
+
+        papertv.setVisibility(View.INVISIBLE);
+
+        throwbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                papertv.setVisibility(View.VISIBLE);
+
+                String writepaper = writetrash.getText().toString();
+                papertv.setText(writepaper);
+
+                writetrash.setText("");
+
+                papertv.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.scale);
+                        papertv.startAnimation(animation);
+                        animation.setAnimationListener(new Animation.AnimationListener(){
+                            public void onAnimationEnd(Animation animation){
+                                trashcan.setImageResource(R.drawable.can);
+                                papertv.setVisibility(View.INVISIBLE);
+                            }
+
+                            public void onAnimationStart(Animation animation){;}
+                            public void onAnimationRepeat(Animation animation){;}
+                        });
+                    }
+                });
+            }
+        });
     }
 }
+
