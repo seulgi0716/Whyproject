26f49e4b3475f6b41339fc347a141bcb3888e2d2
leeseulgi <ijk9877@naver.com>
2020-05-18 18:31:17 +0900

    2020-05-18 commit_5

--- app/src/main/java/com/example/whyproject/RandomPicker.java
+++ app/src/main/java/com/example/whyproject/RandomPicker.java
@@ -5,21 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Random;
+
 
 public class RandomPicker extends AppCompatActivity {
 
-    private ImageButton roll;
+    private ImageButton slot;
+    private ImageView uphandle, downhandle;
+    private TextView randomtv;
+
     String moc[] = {
-            "푹 잔다", "술자리", "운동", "친구와의 대화",
-            "맛있는 음식", "목욕", "산책", "행복한 상상",
-            "TV보기", "영화보기", "유투브 재밌는 영상보기",
-            "눈감고 크게 심호흡", "음악 듣기", "노래방 가기",
-            "청소하기", "쇼핑가기", "스트레칭 하기", "일기에 적어놓기", "게임하기",
-            "아무도 안보는 곳에서 크게 욕하기"
+            "아무 생각 없이 푹 잠에 들기", "부어라~ 마셔라~ 알코올 털어넣기", "땀 쭉쭉 운동", "하하호호 친구들과 수다떨기",
+            "제일 좋아하는 음식 먹기", "목욕", "가볍게 산책", "즐거운 상상",
+            "좋아하는 TV 예능 보기", "영화보기", "유튜브 재밌는 영상보기",
+            "눈감고 크게 심호흡", "음악 듣기", "나는 가수다! 노래방 가기",
+            "쓱싹쓱싹 청소하기", "즐거운 쇼핑하기", "팔다리 쭉쭉 스트레칭 하기", "평소에 즐겨하는 게임",
+            "아무도 안보는 곳에서 크게 욕하기", "땀 뻘뻘! 매운 음식 먹기"
     };
     ArrayList<String> methodOfcancel = new ArrayList<>();
+    Random random = new Random();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,19 +36,30 @@ public class RandomPicker extends AppCompatActivity {
         setContentView(R.layout.activity_random_picker);
         setTitle("알려줘요! 해소 방법");
 
-        roll = findViewById(R.id.roll);
+        slot = findViewById(R.id.slot);
+        uphandle = findViewById(R.id.uphandle);
+        downhandle = findViewById(R.id.downhandle);
+        randomtv = findViewById(R.id.randomtv);
 
+        downhandle.setVisibility(View.INVISIBLE);
+
+        int ran = random.nextInt(moc.length)+1;
 
 
         SoundManager.getInstance();
         SoundManager.initSounds(this);
         SoundManager.loadSounds();
 
-        roll.setOnClickListener(new View.OnClickListener() {
+        slot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 SoundManager.playSound(1,1);
+                downhandle.setVisibility(View.VISIBLE);
+                uphandle.setVisibility(View.INVISIBLE);
+
+
+
 
             }
         });
