fc0205ad60d304bf50fbc070857ebf0b6e6cbe1e
leeseulgi <ijk9877@naver.com>
2020-05-18 09:42:36 +0900

    2020-05-18 commit_1

--- app/src/main/java/com/example/whyproject/WhoteaseActivity.java
+++ app/src/main/java/com/example/whyproject/WhoteaseActivity.java
@@ -4,7 +4,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
@@ -12,12 +11,9 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -28,10 +24,6 @@ import android.widget.Toast;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
-
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 
 public class WhoteaseActivity extends AppCompatActivity {
 
@@ -196,7 +188,8 @@ public class WhoteaseActivity extends AppCompatActivity {
                 LinearLayout.LayoutParams parammargin4 = new LinearLayout.LayoutParams(650, LinearLayout.LayoutParams.MATCH_PARENT);
                 parammargin4.setMargins(0, 20, 0, 0);
 
-                stress_value.setProgressDrawable();
+                stress_value.setProgressDrawable(getResources().getDrawable(R.drawable.seekbar2));
+                stress_value.setThumb(getResources().getDrawable(R.drawable.seekbar1_thumb));
 
                 stress_value.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
                     @Override
